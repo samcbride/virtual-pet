c53ac874d7e6986af8d4e4d853e5691b1ba56cef
Shona McBride <mcbride.shona@gmail.com>
2020-11-07 17:39:11 +0000

    adding the keeping fit section code

--- src/pet.js
+++ src/pet.js
@@ -1,13 +1,23 @@
+const maximumFitness = 10;
+const initialAge = 0;
+const initialHunger = 0;
 function Pet(name) {
     this.name = name;
-    this.age = 0;
-    this.hunger = 0;
-    this.fitness = 10;
+    this.age = initialAge;
+    this.hunger = initialHunger;
+    this.fitness = maximumFitness;
 }
 Pet.prototype.growUp = function() {
     this.age += 1;
     this.hunger += 5;
     this.fitness -= 3;
+}
+Pet.prototype.walk = function() {
+    if((this.fitness + 4) <= 10) {
+        return this.fitness += 4;
+    } else {
+        return this.fitness = 10;
+    };
 };
 
 module.exports = Pet;
