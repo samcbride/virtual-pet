904ef795e064b24669714af257ffea9a9f60e534
Shona McBride <mcbride.shona@gmail.com>
2020-11-07 15:18:28 +0000

    added the tests and code for the  getting older,getting unhealthier section

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -11,11 +11,29 @@ describe('constructor', () => {
       const pet = new Pet('Fido');
       expect(pet.age).toEqual(0);
     });
+    it('has an initial hunger of 0', () => {
+      const pet = new Pet('Fido');
+      expect(pet.hunger).toEqual(0);
+    });
+    it('has an initial fitness of 10', () => {
+      const pet = new Pet('Fido');
+      expect(pet.fitness).toEqual(10);
+    });
   });
 describe('growUp', () => {
   it('increments the age by 1', () => {
     const pet = new Pet('Fido');
     pet.growUp();
     expect(pet.age).toEqual(1);
+  });
+  it('increments the hunger property by 5', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.hunger).toEqual(5);
+  });
+  it('decrements the fitness property by 3', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.fitness).toEqual(7);
   });
 });
